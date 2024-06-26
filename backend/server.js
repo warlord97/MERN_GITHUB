397375c7e95ddd8f5f6f2388c77be3c8530f1343
warlord97 <88074251+warlord97@users.noreply.github.com>
2024-06-27 00:38:58 +0530

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
 import session from "express-session";
+import path from "path";
 
 import "./passport/githubAuth.js";
 
@@ -14,6 +15,8 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
@@ -28,7 +31,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, () => {
-  console.log("server started on http://localhost:5000");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on http://localhost:${PORT}`);
   connectMongoDB();
 });
